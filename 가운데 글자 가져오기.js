5c88be074e5dfcf6c1106e7c322dfd31b8165884
Annakim219 <97003692+Annakim219@users.noreply.github.com>
2022-09-13 22:02:37 +0900

    Create 가운데 글자 가져오기.js

---
+++ 가운데 글자 가져오기.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    let answer =
+      s.length % 2 === 0
+        ? s[s.length / 2 - 1] + s[s.length / 2]
+        : s[Math.floor(s.length / 2)];
+    return answer;
+  }
+  
